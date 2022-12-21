db88829451cb9579b1c43162c27045bc91346e5a
Maya Lynn <mayalynncohen@gmail.com>
2022-12-21 10:27:32 -0800

    Added html and js

---
+++ script.js
@@ -0,0 +1,18 @@
+//1.  Ask user for their selection
+    //prompt the user for thier choice (r,p,s);
+    //TODO: EDGE CASE: what happens if the user doesnt pick r,p, or s?
+    //TODO: EDGE CASE: what happens is casing doesnt match?
+    //TODO: EDGE CASE: what happens if submit empty?
+//2. generate the computer selection
+    //TODO: we need a random selection of r,p,s
+    //TODO: look up random number generator in js;
+//3. compare selections
+    //TODO: if user picks r && comp picks s or user picks s && comp picks p, win;
+    //TODO: if user and comp pick are the same, thats a tie;
+    //TODO: if comp picks r && user picks s or comp picks s && user picks p, loss;
+//4. display result
+    //TODO: show result to user in popup;
+//5. display stats
+    //TODO: create some variable for win, loss and tie,
+    //TODO: update the appropriate variable based on result
+//6. ask to play again
