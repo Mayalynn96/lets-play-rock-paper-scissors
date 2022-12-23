46790797fa8087dc2ae745971e780787a660584c
Maya Lynn <mayalynncohen@gmail.com>
2022-12-23 12:21:03 -0800

    Adding code

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-//1.  Ask user for their selection
-    //prompt the user for thier choice (r,p,s);
-    //TODO: EDGE CASE: what happens if the user doesnt pick r,p, or s?
-    //TODO: EDGE CASE: what happens is casing doesnt match?
-    //TODO: EDGE CASE: what happens if submit empty?
-//2. generate the computer selection
-    //TODO: we need a random selection of r,p,s
-    //TODO: look up random number generator in js;
-//3. compare selections
-    //TODO: if user picks r && comp picks s or user picks s && comp picks p, win;
-    //TODO: if user and comp pick are the same, thats a tie;
-    //TODO: if comp picks r && user picks s or comp picks s && user picks p, loss;
-//4. display result
-    //TODO: show result to user in popup;
-//5. display stats
-    //TODO: create some variable for win, loss and tie,
-    //TODO: update the appropriate variable based on result
-//6. ask to play again
+var options = ["R", "P", "S"]
+var wins = 0;
+var ties = 0;
+var losses = 0;
+
+function play() {
+    var select = prompt("Choose between R (rock), P (paper) or S (scissors)!", "");
+    if(select == "R" || select == "P" || select == "S" || select == "r" || select == "p" || select == "s") {
+        var userChoise = select.toUpperCase();
+        var computersChoice = options[Math.floor(Math.random() * 3)];
+        compare(userChoise, computersChoice);
+        alert("Your score is:\nWins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
+        if (confirm("Do you want to play again?")){
+            play();
+        }
+        alert("Thanks for playing!")
+        return;
+    } else {
+        alert("Please enter either R, P or S");
+        return play();
+    }
+}
+
+play()
+
+function compare(player1, player2) {
+    if((player1 == "R" && player2 == "S") || (player1 == "S" && player2 == "P") || (player1 == "P" && player2 == "R")) {
+        alert("Computer chose " + player2 + " You won!");
+        wins++;
+        return;
+    } else if(player1 == player2) {
+        alert("Computer also chose " + player2 + " It's a tie!");
+        ties++;
+        return;
+    } else {
+        alert("Computer chose " + player2 +  " You lost...");
+        losses++;
+        return;
+    } 
+}
